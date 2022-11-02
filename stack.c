f4bc2fcd09147be55a1bef7a350c5b4849355d9e
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2022-11-03 00:47:44 +0900

    commit

--- stack.c
+++ stack.c
@@ -104,5 +104,116 @@ int main(void)
 プログラムの最初でスタックのデータ数 stack_num を 0 に初期化している。これは絶対に忘れてはならない操作である。
 5 個のデータを push した後に 3回 pop すると，残りのデータは 2 個になる。それに引き続いて push した場合には，残りのデータの後ろに保存される。出力結果を良く見て欲しい。
 最後に，スタックに残っているデータを全て pop して取り出しているが，ここで，スタックが空になっていないかどうかを， stack_num > 0 によって判断している。 
-この部分は、pop の返す値が SUCCESS である間続ける、次のような while ループに書き換えることもできる。
 */
+
+//前節で構築したスタックには，問題点がいくつかある。それらを直していこう。
+
+/*
+①構造体を用いる
+問題点の一つ目は，スタックの本体が stack_num と stack_data という二つの変数からできていることである。
+これら二つは密接に関連しあい一つのスタックを構成するものであるから，それらをまとめて構造体とすることにする。
+*/
+
+#define STACK_SIZE  100
+#define SUCCESS     1
+#define FAILURE     0
+
+typedef int data_t;
+
+typedef struct{
+    int num;
+    data_t  data[STACK_SIZE];
+}stack_t;
+/*
+最初の部分は，定数マクロ定義と，データ型の定義である。
+最後の４行は，構造体を定義する部分 struct {int num; data_t data[STACK_SIZE];} 
+という構造体型を stack_t という新しい名前の型として定義している。
+*/
+
+/*
+②大域変数を用いない
+問題点の二つ目は， push や pop などの関数が，大域変数である stack_num と stack_data に対してのみ働くようになっている点である。
+これでは，スタックを同時に二つ動かすことができない。
+また，大域変数はできるだけ用いない方が良いという観点からも，これは修正すべきである。
+そこで，スッタクの実体を大域変数としてあらかじめ用意するのではなく，必要なときにスタックを何個でも宣言して使えるようにする。
+そのためには， push や pop などの関数に引数として対象とするスタックを引き渡す必要がある。
+また， push や pop はそのスタックの実体に対して操作を施す必要があるので，スタックの値ではなくスタックの実体を指すポインタを引き渡さなければならない。
+*/
+
+/*
+int push(stack_t *stk, data_t push_data) の仕様
+stack_t 型ポインタ stk と、 data_t 型の値 push_data とを引数にとり、stk が指す場所にあるスタックに、データ push_data を追加する。
+追加に成功すると SUCCESS を戻り値として返す。スッタクが満杯で追加に失敗すると，FAILURE を戻り値として返す
+*/
+
+int push(stack_t *stk, data_t push_data)
+{
+    if (stk->num < STACK_SIZE)
+    {
+        stk->data[stk->num] = push_data;
+        stk->num ++;
+    }
+    else{
+        return FAILURE;
+    }
+}
+
+/*
+引数 stk は stack 型の変数を指すポインタである。
+stk が指す場所にある stack 型構造体のメンバ，たとえば num を参照するには， stk->num とすればよい。
+これは (*stk).num と同等であるが，stk->num の方が書くのも楽だし，見た目も良い。
+*/
+
+/*
+int pop(stack_t *stk, data_t *pop_data) の仕様
+stack_t 型ポインタ stk を引数にとり、stk が指す場所にあるスタックからデータを取り出し（stk->num の値を１つ減じ），
+その値を *pop_data に代入し、 SUCCESS を戻り値として返す。スタックが空であった場合は，FAILURE を戻り値として返す以外、何もしない。
+*/
+
+int pop(stack_t *stk, data_t *pop_data)
+{
+    if (stk->num > 0){
+        stk->num --;
+        *pop_data = stk->data[stk->num];
+        return SUCCESS;
+    }
+    else{
+        return FAILURE;
+    }
+}
+
+/*
+void stackPrint(stack_t *stk) の仕様
+stack_t 型ポインタ stk を引数にとり、stk が指す場所にあるスタックにある全てのデータを表示する。
+*/
+
+void stackPrint(stack_t *stk)
+{
+    int i;
+
+    printf("stack [");
+    for (i = 0; i < stk->num; i++){
+        printf("3%d", stk->data[i]);
+    }
+    printf("]\n");
+}
+
+int main(void)
+{
+    stack_t stk;
+    int i,p;
+
+    stk.num = 0; /*スタックのデータ数を0に初期化*/
+    for (i = 1; i <= 4; i++)
+    {
+        push(&stk, i);
+        printf("push %2d :", i);
+        stackPrint(&stk);
+    }
+    while (stk.num > 0)
+    {
+        pop(&stk, &p);
+        printf("pop %2d :", p);
+        stackPrint(&stk);
+    }
+}
