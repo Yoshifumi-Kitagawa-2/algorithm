35440380e2e414b4e388af0324eab161a05eecd3
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2022-11-03 12:38:55 +0900

    commit

--- C言語によるアルゴリズムとデータ構造/第三章：ソーティング/heap_sort.c
+++ C言語によるアルゴリズムとデータ構造/第三章：ソーティング/heap_sort.c
@@ -1,3 +1,53 @@
 /*
-
+ヒープを用いてソーティングアルゴリズムを構成することができる
+ヒープでは最小要素を見つけるのがO(1)時間でできるので、入力n個の要素を全てヒープに挿入して、その後、最小要素を取り出すことを繰り返して並べればソーティングが完了する
 */
+
+void    heapsort()
+{
+    struct heap h;
+    int         i;
+    create(&h);
+    for (i = 1; i <= n; i++)
+        insert(&h, a[i]);
+    for (i = 1; i <= n; i++)
+    {
+        a[i] = findmin(&h);
+        deletemin(&h);
+    }
+}
+
+/*改良版*/
+void    heapify(int i, int j)
+{
+    int k;
+
+    k = 2*i;
+    if (k <= j){
+        if (k != j && a[k]>a[k+1])
+            k++;
+        if (a[i]>a[k]){
+            swap(&a[i], &a[k]);
+            heapify(k,j);
+        }
+    }
+}
+
+void makeheap()
+{
+    int i;
+
+    for (i=n; 1<=i; i--)
+        heapify(1,n);
+}
+
+int main(void)
+{
+    int i;
+
+    makeheap();
+    for(i=n; 2<=0:i--){
+        swap(&a[1], &a[i]);
+        heapify(1,i-1);
+    }
+}
